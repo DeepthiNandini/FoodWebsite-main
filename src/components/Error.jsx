fd3f1b9a402a1b0a03df5de86609b63257d6edda
Deepthi <deepthinandini12@gmail.com>
2024-06-14 12:49:11 +0530

    Initial commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/food-logo.png";
+import { Player } from '@lottiefiles/react-lottie-player';
+import Lottie from "../assets/Nofood.json";
+export default function Error({ title, message }) {
+  return (
+    <div >
+       <Player
+        autoplay
+        loop
+        src={Lottie}
+        className="errorLottie"
+      />
+      <h2 className="error">{title}</h2>
+      <p>{message}
+      </p>
+      
+    </div>
+  );
+}
